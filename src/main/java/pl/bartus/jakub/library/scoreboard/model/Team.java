fb6d2313c4f084f0f46cb00b57b139081cd8e59e
DELL <goxer@o2.pl>
2025-04-09 20:37:25 +0200

    secured team names input

--- src/main/java/pl/bartus/jakub/library/scoreboard/model/Team.java
+++ src/main/java/pl/bartus/jakub/library/scoreboard/model/Team.java
@@ -1,4 +1,10 @@
 package pl.bartus.jakub.library.scoreboard.model;
 
 public record Team(String name) {
+    public Team {
+        if (name != null && !name.isEmpty()) {
+            name = name.toLowerCase();
+            name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+        }
+    }
 }
